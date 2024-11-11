2b8ef6d20ad918071040a6ea3ee30d450f5e1b47
pradnyawante <pradnyawante18@gmail.com>
2024-11-11 19:51:25 +0530

    Add collected static filesNew3

--- pradnya_portfolio/settings.py
+++ pradnya_portfolio/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = os.environ.get('django-insecure-zrj#!6f$08bfr%zq@vobyj6%4f*h225t&62)ckl#)yyaj1(9ln')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('django-insecure-zrj#!6f$08bfr%zq@vobyj6%4f*h225t&62)ckl#)yyaj1(9ln')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
 # ALLOWED_HOSTS = ['127.0.0.1','pradnya-portfolio.onrender.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pradnya-portfolio.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pradnya_portfolio.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
@@ -81,6 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
