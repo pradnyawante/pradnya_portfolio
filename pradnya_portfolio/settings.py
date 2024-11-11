8b8eba45f80886de3ddbf682ddbd579162a41059
pradnyawante <pradnyawante18@gmail.com>
2024-11-11 19:27:09 +0530

    Add collected static filesNew1

--- pradnya_portfolio/settings.py
+++ pradnya_portfolio/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 # ALLOWED_HOSTS = ['127.0.0.1','pradnya-portfolio.onrender.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'wpradnyatech.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pradnya-portfolio.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
