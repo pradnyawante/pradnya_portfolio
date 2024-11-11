77c3206f0c5deff7892d0d9c7151ad7f14a84477
pradnyawante <pradnyawante18@gmail.com>
2024-11-11 19:41:28 +0530

    Add collected static filesNew2

--- pradnya_portfolio/settings.py
+++ pradnya_portfolio/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('django-insecure-zrj#!6f$08bfr%zq@vobyj6%4f*h225t&62)ckl#)yyaj1(9ln')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
